9f777ed9548be1f382875138d90f70179a2208d8
misty-forest <60010521+misty-forest@users.noreply.github.com>
2023-11-24 14:36:39 +0800

    increase test page size

--- liquidity_pool/src/rewards.rs
+++ liquidity_pool/src/rewards.rs
@@ -5,7 +5,7 @@ use soroban_sdk::Env;
 pub(crate) const PAGE_SIZE: u64 = 1000;
 
 #[cfg(test)]
-pub(crate) const PAGE_SIZE: u64 = 2;
+pub(crate) const PAGE_SIZE: u64 = 5;
 
 pub(crate) fn get_rewards_manager(e: &Env) -> Rewards {
     Rewards::new(&e, PAGE_SIZE)
