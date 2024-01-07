b25cfe9d8051dbe7ae8e4efd7f85b1e6581a2a2b
misty-forest <60010521+misty-forest@users.noreply.github.com>
2024-01-07 16:03:54 +0700

    add comments to contract interfaces

--- liquidity_pool_router/src/router_interface.rs
+++ liquidity_pool_router/src/router_interface.rs
@@ -1,15 +1,29 @@
 use soroban_sdk::{Address, BytesN, Env};
 
 pub trait UpgradeableContract {
+    // Get contract version
     fn version() -> u32;
+
+    // Upgrade contract with new wasm code
     fn upgrade(e: Env, new_wasm_hash: BytesN<32>);
 }
 
 pub trait AdminInterface {
+    // Initialize admin user. Will panic if called twice
     fn init_admin(e: Env, account: Address);
+
+    // Set liquidity pool token wasm hash
     fn set_token_hash(e: Env, new_hash: BytesN<32>);
+
+    // Set standard pool wasm hash
     fn set_pool_hash(e: Env, new_hash: BytesN<32>);
+
+    // Set stableswap pool wasm hash for desired amount of tokens
     fn set_stableswap_pool_hash(e: Env, num_tokens: u32, new_hash: BytesN<32>);
+
+    // Configure stableswap init payment: token address, amount and destination address
     fn configure_init_pool_payment(e: Env, token: Address, amount: u128, to: Address);
+
+    // Set reward token address
     fn set_reward_token(e: Env, reward_token: Address);
 }
--- liquidity_pool_stableswap/src/pool_interface.rs
+++ liquidity_pool_stableswap/src/pool_interface.rs
@@ -1,6 +1,7 @@
 use soroban_sdk::{Address, BytesN, Env, Map, Symbol, Val, Vec};
 
 pub trait ManagedLiquidityPool {
+    // Initialize pool completely to reduce calculations cost
     fn initialize_all(
         e: Env,
         admin: Address,
@@ -15,6 +16,7 @@ pub trait ManagedLiquidityPool {
 }
 
 pub trait LiquidityPoolInterfaceTrait {
+    // Get symbolic explanation of pool type.
     fn pool_type(e: Env) -> Symbol;
 
     // Sets the token contract addresses for this pool
@@ -63,6 +65,7 @@ pub trait LiquidityPoolInterfaceTrait {
         out_min: u128,
     ) -> u128;
 
+    // Estimate amount of coins to retrieve using swap function
     fn estimate_swap(e: Env, in_idx: u32, out_idx: u32, in_amount: u128) -> u128;
 
     // Withdraw coins from the pool.
@@ -71,20 +74,39 @@ pub trait LiquidityPoolInterfaceTrait {
     // Returns a list of the amounts for each coin that was withdrawn.
     fn withdraw(e: Env, user: Address, share_amount: u128, min_amounts: Vec<u128>) -> Vec<u128>;
 
+    // Get dictionary of basic pool information: type, fee, special parameters if any.
     fn get_info(e: Env) -> Map<Symbol, Val>;
 }
 
 pub trait UpgradeableContractTrait {
+    // Get contract version
     fn version() -> u32;
+
+    // Upgrade contract with new wasm code
     fn upgrade(e: Env, new_wasm_hash: BytesN<32>);
 }
 
 pub trait RewardsTrait {
     // todo: move rewards configuration to gauge
+
+    // Initialize rewards settings: token address and storage address
+    // from which transfer will be made on claim
     fn initialize_rewards_config(e: Env, reward_token: Address, reward_storage: Address);
+
+    // Configure rewards for pool. Every second tps of coins
+    // being distributed across all liquidity providers
+    // after expired_at timestamp distribution ends
     fn set_rewards_config(e: Env, admin: Address, expired_at: u64, tps: u128);
+
+    // Get rewards status for the pool,
+    // including amount available for the user
     fn get_rewards_info(e: Env, user: Address) -> Map<Symbol, i128>;
+
+    // Get amount of reward tokens available for the user to claim.
     fn get_user_reward(e: Env, user: Address) -> u128;
+
+    // Claim reward as a user.
+    // returns amount of tokens rewarded to the user
     fn claim(e: Env, user: Address) -> u128;
 }
 
