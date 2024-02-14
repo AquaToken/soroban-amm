8c9dc0a16f839f300fac0821b98d030265dff8ac
misty-forest <60010521+misty-forest@users.noreply.github.com>
2024-02-14 12:13:22 +0800

    remove unused function

--- utils/src/utils.rs
+++ utils/src/utils.rs
@@ -1,15 +1,5 @@
 use soroban_sdk::{Address, Vec};
 
-pub fn sort(a: &Address, b: &Address) -> (Address, Address) {
-    if a == b {
-        panic!("a and b can't be the same")
-    }
-    match a < b {
-        true => (a.clone(), b.clone()),
-        false => (b.clone(), a.clone()),
-    }
-}
-
 pub fn check_vec_ordered(addresses: &Vec<Address>) -> bool {
     for i in 0..addresses.len() - 1 {
         if addresses.get(i).unwrap() > addresses.get(i + 1).unwrap() {
