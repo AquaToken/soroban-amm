998bc1bbd367139862c4895237ab039d407eef09
misty-forest <60010521+misty-forest@users.noreply.github.com>
2024-06-27 12:28:16 +0800

    add upgrade to liquidity calculator

--- liquidity_pool_liquidity_calculator/src/interface.rs
+++ liquidity_pool_liquidity_calculator/src/interface.rs
@@ -1,8 +1,16 @@
-use soroban_sdk::{Address, Env, Vec, U256};
+use soroban_sdk::{Address, BytesN, Env, Vec, U256};
 
 pub trait Calculator {
     fn init_admin(e: Env, account: Address);
     fn set_pools_plane(e: Env, admin: Address, plane: Address);
     fn get_pools_plane(e: Env) -> Address;
     fn get_liquidity(e: Env, pools: Vec<Address>) -> Vec<U256>;
+}
+
+pub trait UpgradeableContractTrait {
+    // Get contract version
+    fn version() -> u32;
+
+    // Upgrade contract with new wasm code
+    fn upgrade(e: Env, new_wasm_hash: BytesN<32>);
 }
