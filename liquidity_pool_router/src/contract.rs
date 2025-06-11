6d8ead9c2d66141bd8617c6f916c2c3db6359f51
misty-forest <60010521+misty-forest@users.noreply.github.com>
2025-06-11 12:23:47 +0400

    bump pools version

--- liquidity_pool_router/src/contract.rs
+++ liquidity_pool_router/src/contract.rs
@@ -404,7 +404,7 @@ impl UpgradeableContract for LiquidityPoolRouter {
     //
     // The version of the contract as a u32.
     fn version() -> u32 {
-        150
+        160
     }
 
     // Commits a new wasm hash for a future upgrade.
