357f5d5a34175e2780607f62265d87f5decab128
misty-forest <60010521+misty-forest@users.noreply.github.com>
2026-03-10 16:11:50 +0100

    add claim_fees event description

--- liquidity_pool_concentrated/src/events.rs
+++ liquidity_pool_concentrated/src/events.rs
@@ -1,5 +1,17 @@
 use soroban_sdk::{contractevent, Address, Env};
 
+// topics
+// [
+//   "claim_fees": Symbol,      // event identifier
+//   owner: Address             // address of account/contract that initiated the claim
+//   token0: Address,           // Address of token0
+//   token1: Address,           // Address of token1
+// ]
+// body
+// [
+//   amount0: i128,              // amount of token0 claimed
+//   amount1: i128,              // amount of token1 claimed
+// ]
 #[contractevent(data_format = "vec")]
 pub struct ClaimFees {
     #[topic]
